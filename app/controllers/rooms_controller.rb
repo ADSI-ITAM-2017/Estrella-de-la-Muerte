2f96ed7f892b1984a99f2b94a0038cebacae1a64
Pablo <paesqg@gmail.com>
2017-05-16 00:21:47 -0500

    hasta hipervinculo en propiedades -falta desplegar la reseña-

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,36 @@
 class RoomsController < ApplicationController
+  before_action :find_room, only: [:show, :edit, :update, :destroy]
+
   def index
+    @rooms = Room.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
 
   end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:nombre, :precio, :lat, :long, :ubicacion, :reseña)
+  end
+
+  def find_room
+    @room = Room.find(params[:id])
+  end
+
 end
